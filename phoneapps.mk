32229fceb8844290f4955e63a9013bdf621960e0
nian0114 <268078545@qq.com>
2015-05-08 02:59:19 +0800

    update miui v6

---
+++ phoneapps.mk
@@ -0,0 +1,38 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+BasicDreams \
+Bluetooth \
+BluetoothExt \
+CellBroadcastReceiver \
+CertInstaller \
+DocumentsUI \
+HTMLViewer \
+KeyChain \
+LatinIME \
+Nfc \
+PacProcessor \
+PhotoTable \
+PrintSpooler \
+PicoTts \
+Stk \
+UserDictionaryProvider \
+WAPPushManager \
+LockClock
+
+private-phone-priv-apps := \
+BackupRestoreConfirmation \
+FusedLocation \
+InputDevices \
+DefaultContainerService \
+DownloadProvider \
+ExternalStorageProvider \
+MediaProvider \
+OneTimeInitializer \
+ProxyHandler \
+SharedStorageBackup \
+Shell \
+Tag \
+VpnDialogs \
+WallpaperCropper 
